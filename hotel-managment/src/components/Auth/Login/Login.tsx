01870c412d40f908f1696dad1845fe022845af21
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-02-07 12:49:44 +0200

    Login register validations

--- hotel-managment/src/components/Auth/Login/Login.tsx
+++ hotel-managment/src/components/Auth/Login/Login.tsx
@@ -3,6 +3,9 @@ import { Button } from "../../Shared/Button/Button"
 import { InputField } from "../../Shared/InputField/InputField"
 import styles from './Login.module.scss'
 import { useForm } from "../../../hooks/useForm"
+import { useFormValidation } from "../../../hooks/useFormValidation"
+import { useLoginValidations } from "./LoginHook"
+
 
 export const Login = () => {
 
@@ -15,16 +18,43 @@ export const Login = () => {
         password: '',
     },onFormSubmit)
 
+    
+    const {onBlurHandler,onFocusHandler,validationValues} = useFormValidation({
+        hotelCode : false,
+        password: false
+    })
+
+   const {isCodeValid, isPasswordValid,disableButton} = useLoginValidations(formValues,validationValues)
+
     return (
         <div className={styles["login"]}>
     
-
-            <h1>Welocome back!</h1>
+            <h1>Welcome back!</h1>
 
             <form className={styles["login-form"]} onSubmit={onSubmit}>
-                <InputField onChange={(e) => onChangeHandler(e)} name="hotelCode" value={formValues.hotelCode} type="password">Hotel Code</InputField>
-                <InputField onChange={(e) => onChangeHandler(e)} name="password" value={formValues.password} type="password">Password</InputField>
-                <Button>Login</Button>
+
+                <InputField 
+                onChange={(e) => onChangeHandler(e)} 
+                name="hotelCode" 
+                value={formValues.hotelCode} 
+                type="password"
+                onBlurHandler={() => onBlurHandler('hotelCode')}
+                onFocusHandler={() => onFocusHandler('hotelCode')}
+                isValid = {{ boolean: !isCodeValid, errorMessage: 'Hotel Code should be at least 3 characters long'}}
+                >Hotel Code</InputField>
+
+                <InputField 
+                onChange={(e) => onChangeHandler(e)} 
+                name="password" 
+                value={formValues.password} 
+                type="password"
+                onBlurHandler={() => onBlurHandler('password')}
+                onFocusHandler={() => onFocusHandler('password')}
+                isValid={{boolean: !isPasswordValid, errorMessage: 'Password should be at least 5 characters long' }}
+                >Password</InputField>
+
+                <Button disable={disableButton}>Login</Button>
+
             </form>
             <p>Don't have a registration? <Link  to="/register">Sign up.</Link></p>
         </div>
--- hotel-managment/src/components/Auth/Register/Register.tsx
+++ hotel-managment/src/components/Auth/Register/Register.tsx
@@ -3,6 +3,8 @@ import { Button } from '../../Shared/Button/Button'
 import { InputField } from '../../Shared/InputField/InputField'
 import styles from './Register.module.scss'
 import { useForm } from '../../../hooks/useForm'
+import { useFormValidation } from '../../../hooks/useFormValidation'
+import { useRegisterValidations } from './RegisterHook'
 
 export const Register = () => {
 
@@ -20,6 +22,28 @@ export const Register = () => {
         repeatPassword:''
     },onFormSubmit)
 
+
+    const {onBlurHandler,onFocusHandler,validationValues} = useFormValidation({
+        email : false,
+        phoneNumber : false,
+        location : false,
+        hotelName : false,
+        password: false,
+        repeatPassword : false,
+
+    })
+
+    const { 
+        isEmailValid, 
+        isHotelNameValid, 
+        isLocationValid, 
+        isPasswordValid, 
+        isPhoneNumberValid, 
+        isRepeatPasswordValid,
+        disableButton
+    } = useRegisterValidations(formValues, validationValues)
+
+    
     return (
         <div className={styles["login"]}>
     
@@ -27,13 +51,62 @@ export const Register = () => {
             <h1>Welcome!</h1>
 
             <form className={styles["login-form"]} onSubmit={onSubmit}>
-                <InputField onChange={(e) => onChangeHandler(e)} value={formValues.email} name='email'>E-mail</InputField>
-                <InputField onChange={(e) => onChangeHandler(e)} value={formValues.phoneNumber} name='phoneNumber' type='number'>Phone Number</InputField>
-                <InputField onChange={(e) => onChangeHandler(e)} value={formValues.location} name='location'>Country, City, Address</InputField>
-                <InputField onChange={(e) => onChangeHandler(e)} value={formValues.hotelName} name='hotelName'>Hotel Name</InputField>
-                <InputField onChange={(e) => onChangeHandler(e)} value={formValues.password} name='password' type='password'>Password</InputField>
-                <InputField onChange={(e) => onChangeHandler(e)} value={formValues.repeatPassword} name='repeatPassword' type='password'>Repeat-Password</InputField>
-                <Button>Register</Button>
+                <InputField 
+                onChange={(e) => onChangeHandler(e)} 
+                value={formValues.email} 
+                name='email'
+                onBlurHandler={() => onBlurHandler('email')}
+                onFocusHandler={() => onFocusHandler('email')}
+                isValid={{boolean: !isEmailValid, errorMessage: 'Invalid Email'}}
+                >E-mail</InputField>
+                
+                <InputField 
+                onChange={(e) => onChangeHandler(e)} 
+                value={formValues.phoneNumber} 
+                name='phoneNumber' type='number'
+                onBlurHandler={() => onBlurHandler('phoneNumber')}
+                onFocusHandler={() => onFocusHandler('phoneNumber')}
+                isValid={{boolean: !isPhoneNumberValid, errorMessage: 'Phone Number should be at least 7 characters long'}}
+                >Phone Number</InputField>
+
+                <InputField 
+                onChange={(e) => onChangeHandler(e)} 
+                value={formValues.location} 
+                name='location'
+                onBlurHandler={() => onBlurHandler('location')}
+                onFocusHandler={() => onFocusHandler('location')}
+                isValid={{boolean: !isLocationValid, errorMessage: 'Location should be at least 3 characters long'}}
+
+                >Country, City, Address</InputField>
+
+                <InputField 
+                onChange={(e) => onChangeHandler(e)} 
+                value={formValues.hotelName} 
+                name='hotelName'
+                onBlurHandler={() => onBlurHandler('hotelName')}
+                onFocusHandler={() => onFocusHandler('hotelName')}
+                isValid={{boolean: !isHotelNameValid, errorMessage: 'Hotel Name should be at least 3 characters long'}}
+                >Hotel Name</InputField>
+
+                <InputField 
+                onChange={(e) => onChangeHandler(e)} 
+                value={formValues.password}
+                name='password' type='password'
+                onBlurHandler={() => onBlurHandler('password')}
+                onFocusHandler={() => onFocusHandler('password')}
+                isValid={{boolean: !isPasswordValid, errorMessage: 'Password should be at least 5 characters long'}}
+                >Password</InputField>
+
+                <InputField 
+                onChange={(e) => onChangeHandler(e)} 
+                value={formValues.repeatPassword} 
+                name='repeatPassword' type='password'
+                onBlurHandler={() => onBlurHandler('repeatPassword')}
+                onFocusHandler={() => onFocusHandler('repeatPassword')}
+                isValid={{boolean: !isRepeatPasswordValid, errorMessage: 'Passwords not matching'}}
+                >Repeat-Password</InputField>
+
+                <Button disable={disableButton}>Register</Button>
             </form>
             <p>Already registered your hotel? <Link to="/login">Sign in.</Link></p>
         </div>
