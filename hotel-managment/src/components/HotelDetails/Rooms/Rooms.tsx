a2e645df3bff8977909d866b445ba1783c2a3216
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-02-21 18:33:09 +0200

    Room layout

--- hotel-managment/src/components/HotelDetails/Rooms/Rooms.tsx
+++ hotel-managment/src/components/HotelDetails/Rooms/Rooms.tsx
@@ -1,7 +1,33 @@
+import { useState } from "react"
+import { Dropdown } from "../../Shared/Dropdown/Dropdown"
+import { RoomItem } from "./RoomItem/RoomItem"
+import styles from './Rooms.module.scss'
+import { Button } from "../../Shared/Button/Button"
+
 export const Rooms = () => {
+    const floorTestOptions = [1,2,3,4]
+
+    const roomTestOptions = [101,102,103,104,105,106,107,101,102,103,104,105,106,107,101,102,103,104,105,106,107,101,102,103,104,105,106,107]
+
+    const [rooms,setRooms ] = useState(roomTestOptions)
+
     return (
         <>
-        <h1>Rooms</h1>
+        <div className={styles["rooms"]}>
+            <h1>Rooms</h1>
+            <div className={styles["dropdowns"]}>
+                <Dropdown options={floorTestOptions}>Floor</Dropdown>
+                <Dropdown options={roomTestOptions}>Room</Dropdown>
+            </div>
+            {rooms && 
+                <div className={styles["rooms-list"]}>
+                    {rooms.map(room => <RoomItem key={room} number={room}/>)}    
+            </div>
+            }
+            <div className={styles["buttons"]}>
+                <Button>Add a Floor</Button>
+            </div>
+        </div>
         </>
     )
 }
