cef5694ffac1fc8f5f3b239dbf6ea99454751c9f
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-02-23 18:24:47 +0200

    Toast notification

--- hotel-managment/src/components/Shared/ToastNotification/ToastNotification.tsx
+++ hotel-managment/src/components/Shared/ToastNotification/ToastNotification.tsx
@@ -1,7 +1,7 @@
 import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './ToastNotification.module.scss'
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 // import {  fa-faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
 
 interface Props {
@@ -9,23 +9,21 @@ interface Props {
 }
 
 export const ToastNotification: React.FC<Props> = ({text}) => {
-    const [showNotification, setShowNotification] = useState(true)
+    // const [showNotification, setShowNotification] = useState(true)
     
-        useEffect(() => {
-            const timer = setTimeout(() => {
-                setShowNotification(false);
-            }, 3000); 
+    //     useEffect(() => {
+    //         const timer = setTimeout(() => {
+    //             setShowNotification(false);
+    //         }, 3000); 
 
-            return () => clearTimeout(timer);
-        })
+    //         return () => clearTimeout(timer);
+    //     })
     return (
         <>
-        {showNotification && 
         <div className={styles["toast-notification"]}>
             <FontAwesomeIcon size='2x' icon={faTriangleExclamation}></FontAwesomeIcon>
             <p>{text}</p>
         </div>
-        }
         </>
     )
 }
