094d6cfa80b02b1491cb6d946f008e037a052477
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-02-23 18:25:36 +0200

    tab button acitve highlight

--- hotel-managment/src/components/HotelDetails/TabButton/TabButton.tsx
+++ hotel-managment/src/components/HotelDetails/TabButton/TabButton.tsx
@@ -9,9 +9,12 @@ interface Props {
 export const TabButton:React.FC<Props> = ({children}) => {
     const navigate = useNavigate()
     const params = useParams()
-    
+
+
     return (
-        <button className={styles["tab"]} onClick={() => navigate(`/hotels/${params.id}/${String(children).toLocaleLowerCase()}`)}>
+        <button style={params['*']!.includes(String(children).toLocaleLowerCase()) ? {color: "#4844bf" } : {}} 
+        className={`${styles["tab"]} ${params['*']!.includes(String(children).toLocaleLowerCase()) ? styles['active'] : ''}`} 
+        onClick={() => navigate(`/hotels/${params.id}/${String(children).toLocaleLowerCase()}`)}>
             {children}
         </button>
     )
