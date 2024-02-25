60d8d25511af84fa7c5ad22f57a08756cdb8ae7b
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-02-25 21:27:20 +0200

    rooms fetch/add floors and rooms

--- hotel-managment/src/components/HotelDetails/Rooms/Rooms.tsx
+++ hotel-managment/src/components/HotelDetails/Rooms/Rooms.tsx
@@ -1,6 +1,5 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Dropdown } from "../../Shared/Dropdown/Dropdown"
-import { RoomItem } from "./RoomItem/RoomItem"
 import styles from './Rooms.module.scss'
 import { Button } from "../../Shared/Button/Button"
 import { Modal } from "../../Shared/Modal/Modal"
@@ -8,20 +7,29 @@ import { InputField } from "../../Shared/InputField/InputField"
 import { ToastNotification } from "../../Shared/ToastNotification/ToastNotification"
 import { useForm } from "../../../hooks/useForm"
 import { useRooms } from "./RoomsHook"
+import { RoomsList } from "./RoomsList/RoomsList"
+import { floorServiceFactory } from "../../../services/floors"
+import { useParams } from "react-router-dom"
+import { roomServiceFactory } from "../../../services/room"
 
 export const Rooms = () => {
-    const floorTestOptions = [1,2,3,4]
-
-    const roomTestOptions = [101,102,103,104,105,106,107,101,102,103,104,105,106,107,101,102,103,104,105,106,107,101,102
-        ,103,104,105,106,107,101,102,103,104,105,106,107,101,102,103,104,105,106,107,101,102,103,104,105,106,107,101,102
-        ,103,104,105,106,107101,102,103,104,105,106,107,101,102,103,104,105,106,107,101,102,103,104,105,106,107,101,102
-        ,103,104,105,106,107101,102,103,104,105,106,107,101,102,103,104,105,106,107,101,102,103,104,105,106,107,101,102
-        ,103,104,105,106,107]
     
-        const [rooms,setRooms ] = useState(roomTestOptions)
+        const [rooms,setRooms ] = useState<{roomNumber: number,floorId: string}[]>([])
         const [roomModal,setRoomModal] = useState(false)
         const [floorModal, setFloorModal] = useState(false)
         const [toastText, setToastText] = useState('')
+        const [floors, setFloors ] = useState<{floorNumber:number,id:string }[]>([]) 
+
+        const floorService = floorServiceFactory()
+        const roomService = roomServiceFactory()
+
+        const params = useParams()
+
+        useEffect(() => {
+            floorService.get(String(params.id))
+            .then( data => setFloors(data.floors))
+            .catch(err => console.error(err))
+        },[])
     
         const {formValues,onChangeHandler} = useForm({
         floorValue: '',
@@ -29,10 +37,23 @@ export const Rooms = () => {
         roomNumber: '',
     },() => {})
 
+    useEffect (() => {
+        if(formValues.floorValue == ''){
+            setRooms([])
+            return;
+        }
+            const floorId = floors.find(x => String(x.floorNumber) == formValues.floorValue)?.id
+            if(floorId){
+                roomService.get(floorId)
+                .then(data => setRooms(data.rooms))
+            }
+    },[formValues.floorValue])
+
     const {
         onAddRoomClick,
-        onAddRoomHandler
-    } = useRooms(setRoomModal,formValues,setToastText)
+        onAddRoomHandler,
+        onAddFloor
+    } = useRooms(setRoomModal,formValues,setToastText,setRooms,setFloors,floors,setFloorModal)
 
     return (
         <>
@@ -54,8 +75,8 @@ export const Rooms = () => {
             { floorModal && 
                 <Modal stateSetter={setFloorModal} title="Are you sure you want to add a Floor?">
                     <div className={styles["floor-modal-buttons"]}>
-                    <Button width="8rem">Yes</Button>
-                    <Button width="8rem" onClick={() =>setFloorModal(false)}>Cancel</Button>
+                    <Button width="8rem" onClick={onAddFloor}>Yes</Button>
+                    <Button width="8rem" onClick={() => setFloorModal(false)}>Cancel</Button>
                     </div>
                 </Modal>
             }
@@ -67,14 +88,12 @@ export const Rooms = () => {
         <div className={styles["rooms"]}>
             <h1>Rooms</h1>
             <div className={styles["dropdowns"]}>
-                <Dropdown onChange={onChangeHandler} name="floorValue" value={formValues.floorValue} options={floorTestOptions}>Floor</Dropdown>
+                <Dropdown onChange={onChangeHandler} name="floorValue" value={formValues.floorValue} options={floors}>Floor</Dropdown>
                 {/* <Dropdown onChange={onChangeHandler} name="roomValue" value={formValues.roomValue} options={roomTestOptions}>Room</Dropdown> */}
                 {/* DONT REMOVE YET */}
             </div>
             {rooms && 
-                <div className={styles["rooms-list"]}>
-                    {rooms.map(room => <RoomItem key={room} number={room}/>)}    
-            </div>
+                <RoomsList rooms={rooms}/>
             }
             <div className={styles["buttons"]}>
                 <Button width="90%" onClick={() => setFloorModal(true)}>Add a Floor</Button>
