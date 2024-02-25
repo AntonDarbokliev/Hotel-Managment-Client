651eb2605d0fb25fdbee185b40269b468a8fa48c
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-02-25 21:27:35 +0200

    Rooms list

---
+++ hotel-managment/src/components/HotelDetails/Rooms/RoomsList/RoomsList.tsx
@@ -0,0 +1,14 @@
+import { RoomItem } from "../RoomItem/RoomItem"
+import styles from './RoomsList.module.scss';
+
+interface Props {
+    rooms: {floorId:string,roomNumber:number}[]
+}
+
+export const RoomsList:React.FC<Props> = ({rooms}) => {
+    return (
+        <div className={styles["rooms-list"]}>
+            {rooms.map(room => <RoomItem key={room.roomNumber} number={room.roomNumber}/>)}    
+        </div>
+    )
+}
