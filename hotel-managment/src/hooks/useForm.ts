68440478bb6ca0833f672bc303ebfcaa42aebe1c
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-02-25 21:27:57 +0200

    adjustments

--- hotel-managment/src/hooks/useForm.ts
+++ hotel-managment/src/hooks/useForm.ts
@@ -1,13 +1,13 @@
-import { ChangeEvent, FormEvent, useState } from "react"
+import { ChangeEvent, FormEvent, useCallback, useState } from "react"
 
 
 export const useForm = <T>(initialValue : T,onSubmitHandler : (values: T) => void) => {
     const [formValues, setFormValues ] = useState(initialValue)
 
-    const onChangeHandler = (e: FormEvent | ChangeEvent) => {
+    const onChangeHandler = useCallback( (e: FormEvent | ChangeEvent) => {
         const element = e.target as HTMLInputElement
         setFormValues((state) => ({...state, [element.name] : element.value }))
-    }
+    },[])
 
     const onSubmit = (e: FormEvent) => {
         e.preventDefault()
