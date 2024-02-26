51b687145554bcc3fa6e51980fb9b14a6b4f5e09
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-02-26 20:33:17 +0200

    spinner and error handling on login

--- hotel-managment/src/components/Auth/Login/Login.tsx
+++ hotel-managment/src/components/Auth/Login/Login.tsx
@@ -8,36 +8,43 @@ import { useLoginValidations } from "./LoginHook";
 import { authServiceFactory } from "../../../services/auth.ts";
 import { useState } from "react";
 import { useAuthStore } from "../../../stores/Auth.ts";
+import { useLoading } from "../../../hooks/useLoading.ts";
+import Spinner from "../../Shared/LoadSpinner/LoadSpinner.tsx";
+import { ToastNotification } from "../../Shared/ToastNotification/ToastNotification.tsx";
 
 const authService = authServiceFactory();
 
 
 export const Login = () => {
-  const [loginError, setLoginError] = useState<string | null>(null);
+
+  const [toastText,setToastText] = useState('')
   const updateUser = useAuthStore((s) => s.updateUser)
 
   const navigate = useNavigate();
+
+  const {isLoading,requestWithLoading} =  useLoading()
   
   const onFormSubmit = async () => {
     const data = new FormData();
     data.append("LoginCode", formValues.hotelCode);
     data.append("Password", formValues.password);
-
     try {
-      const response = await authService.login(data);
+      const response =  await requestWithLoading( () => authService.login(data));
       const token = response.success; 
       localStorage.setItem("token", token);
       
       updateUser()
       
-      navigate("/");
+      navigate("/hotels");
     } catch (error) {
-      console.error("Login failed", error);
-      setLoginError(String(error));
+      if(typeof error == 'object'){
+        setToastText((error as {error:string}).error)
+        resetForm()
+      }
     }
   };
 
-  const { formValues, onChangeHandler, onSubmit } = useForm(
+  const { formValues, onChangeHandler, onSubmit,resetForm } = useForm(
     {
       hotelCode: "",
       password: "",
@@ -59,6 +66,12 @@ export const Login = () => {
 
   return (
     <div className={styles["login"]}>
+      {toastText !== '' && 
+      <ToastNotification text={toastText} timer={3000} setText={setToastText}></ToastNotification>
+      }
+
+      {!isLoading && 
+      <>
       <h1>Welcome back!</h1>
 
       <form className={styles["login-form"]} onSubmit={onSubmit}>
@@ -91,7 +104,6 @@ export const Login = () => {
         >
           Password
         </InputField>
-        {loginError && <p className={styles.error}>{loginError}</p>}
 
         <Button width='10rem' disable={disableButton}>Login</Button>
       </form>
@@ -99,6 +111,12 @@ export const Login = () => {
       <p>
         Don't have a registration? <Link to="/register">Sign up.</Link>
       </p>
+      </>
+      }
+
+      {isLoading && 
+      <Spinner/> 
+      }
     </div>
   );
 };
