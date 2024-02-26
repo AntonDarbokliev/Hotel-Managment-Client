7ed8ed9d8fe088bf62a231496593d9c678e29949
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-02-26 17:16:48 +0200

    useLoading

--- hotel-managment/src/hooks/useLoading.ts
+++ hotel-managment/src/hooks/useLoading.ts
@@ -5,13 +5,14 @@ export const useLoading = () => {
     const [error, setError ] = useState<Error | null>(null)
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const requestMethod = async (callback: () => Promise<any>) => {
+    const requestWithLoading = async (callback: () => Promise<any>) => {
         setIsLoading(true)
         try {
             const result = await callback()
             setIsLoading(false)
             return result
         } catch (error) {
+            console.log(error)
             if(error instanceof Error)
             setError(error)
             setIsLoading(false)
@@ -21,6 +22,6 @@ export const useLoading = () => {
     return {
         isLoading,
         error,
-        requestMethod
+        requestWithLoading
     }
 }
