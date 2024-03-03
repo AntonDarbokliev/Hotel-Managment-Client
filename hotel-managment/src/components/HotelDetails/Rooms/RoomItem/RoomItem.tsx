d676a265cde1f1827255ec9ca41811e102df2dc3
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-03-03 18:36:10 +0200

    Redirect Room item

--- hotel-managment/src/components/HotelDetails/Rooms/RoomItem/RoomItem.tsx
+++ hotel-managment/src/components/HotelDetails/Rooms/RoomItem/RoomItem.tsx
@@ -1,13 +1,21 @@
+import { useNavigate } from 'react-router-dom'
 import styles from './RoomItem.module.scss'
 
 interface Props {
-    number: number
+    number: number,
+    id: string,
 }
 
-export const RoomItem:React.FC<Props> = ({number}) => {
+export const RoomItem:React.FC<Props> = ({number,id}) => {
+
+    const navigate = useNavigate()
+
+    const redirect = () => {
+        navigate('/room/' + id + '/info')
+    }
     return (
-        <div className={styles["room-item"]}>
-            <h1>{number}</h1>
+        <div className={styles["room-item"]} onClick={redirect}>
+            <h1 >{number}</h1>
         </div>
     )
 }
--- hotel-managment/src/components/HotelDetails/Rooms/RoomsList/RoomsList.tsx
+++ hotel-managment/src/components/HotelDetails/Rooms/RoomsList/RoomsList.tsx
@@ -3,13 +3,14 @@ import { RoomItem } from "../RoomItem/RoomItem"
 import styles from './RoomsList.module.scss';
 
 interface Props {
-    rooms: {floorId:string,roomNumber:number}[]
+    rooms: {id:string,roomNumber:number}[]
 }
 
 export const RoomsList:React.FC<Props> = memo(({rooms}) => {
+    console.log(rooms)
     return (
         <div className={styles["rooms-list"]}>
-            {rooms.map(room => <RoomItem key={room.roomNumber} number={room.roomNumber}/>)}    
+            {rooms.map(room => <RoomItem id={room.id} key={room.roomNumber} number={room.roomNumber}/>)}    
         </div>
     )
 })
