d1b2379e2f49afd63a4bd243f63adae1ee15f9a0
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-03-03 18:51:46 +0200

    Clean up

--- hotel-managment/src/components/RoomDetails/RoomInfo/AmenityModal/AmenityModal.tsx
+++ hotel-managment/src/components/RoomDetails/RoomInfo/AmenityModal/AmenityModal.tsx
@@ -4,67 +4,32 @@ import { Modal } from "../../../Shared/Modal/Modal"
 import { AmenityCard } from "../AmenityCard/AmenityCard"
 import { faVault } from '@fortawesome/free-solid-svg-icons'
 import { faBath } from '@fortawesome/free-solid-svg-icons'
-
 import styles from './AmenityModal.module.scss'
-import { useState } from "react"
-import { useForm } from "../../../../hooks/useForm"
 import { Amenity } from "../../../../types/AmenityType"
-import { useParams } from "react-router-dom"
-import { roomServiceFactory } from "../../../../services/room"
+import { Room } from "../../../../types/RoomType"
+import { useAmenityModal } from "./AmenityModalHook"
 
 
 interface Props {
-    modalSetter: React.Dispatch<React.SetStateAction<boolean>>
+    modalSetter: React.Dispatch<React.SetStateAction<boolean>>,
+    currentAmenities: Amenity[],
+    roomSetter: React.Dispatch<React.SetStateAction<Room | undefined>>
 }
 
-export const AmenityModal:React.FC<Props> = ({modalSetter}) => {
+const preDefinedAmenities = ['Bathtub','Safe']
 
-    const [amenities, setAmenities] = useState<Amenity[]>([])
-    // const [otherInput, setOtherInput ] = useState('')
-    const [otherAmenities, setOtherAmenities] = useState<Amenity[]>([])
-    const params =  useParams()
+export const AmenityModal:React.FC<Props> = ({modalSetter,currentAmenities,roomSetter}) => {
 
-    const roomService = roomServiceFactory()
+    const initialAmenities = currentAmenities.filter(x => preDefinedAmenities.includes(x.name))
+    const initialOtherAmenities = currentAmenities.filter(x => !preDefinedAmenities.includes(x.name))
 
-    const onAdd = () => {
-
-        const amenity = {
-            name: formValues.other,
-            roomId: params.id!
-        }
-
-
-        if(!otherAmenities.some(x => x.name === amenity.name) && amenity.name !== '') {
-            setOtherAmenities(state => [...state,amenity])
-            resetForm()
-        }
-    }
-
-    const {formValues,onChangeHandler,onSubmit,resetForm } = useForm({
-        other: '',
-    },onAdd)
-
-
-    const amenityOnClick = (e) => {
-        
-        const amenity:Amenity = {
-            name: e.currentTarget.id,
-            roomId: params.id!
-        }
-        
-        if(!amenities.some(x => x.name === amenity.name)){
-            setAmenities(state => [...state,amenity])
-        }else {
-            setAmenities(state => state.filter(x => x.name !== amenity.name))
-
-        }
-    }
-
-    const onSave = () => {
-        const allAmenities = [...amenities,...otherAmenities]
-        roomService.addAmenities(allAmenities)
-        console.log(allAmenities)
-    }
+    const { amenityOnClick, 
+        onChangeHandler,
+        onSave,
+        onSubmit,
+        amenities,
+        otherAmenities,
+        formValues } = useAmenityModal({modalSetter,initialAmenities,initialOtherAmenities,roomSetter})
 
     return (
         <Modal title="Add Amenities" stateSetter={modalSetter}>
@@ -73,13 +38,10 @@ export const AmenityModal:React.FC<Props> = ({modalSetter}) => {
 
                 <AmenityCard amenities={amenities} onClick={amenityOnClick} name="Safe" icon={faVault}></AmenityCard>
                 
-                
-                {/* <div> */}
                     <form action=""className={styles["other"]} onSubmit={onSubmit}>
                         <InputField name="other" value={formValues.other} onChange={onChangeHandler}>Other: </InputField>
                         <Button width="2rem" >+</Button>  
                     </form>
-                {/* </div> */}
 
                 <div className={styles["other-amenities"]}>
                     {otherAmenities.map(x => <p key={x.name}>{x.name}</p>)}
