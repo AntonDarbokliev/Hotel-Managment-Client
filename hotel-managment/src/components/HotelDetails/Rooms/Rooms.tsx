0eeb4f24b69f2f8a77a2684b9f78939bde9b20fc
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-03-07 23:41:27 +0200

    Clean up

--- hotel-managment/src/components/HotelDetails/Rooms/Rooms.tsx
+++ hotel-managment/src/components/HotelDetails/Rooms/Rooms.tsx
@@ -1,72 +1,59 @@
-import {useEffect, useState } from "react"
+import { useState } from "react"
 import { Dropdown } from "../../Shared/Dropdown/Dropdown"
 import styles from './Rooms.module.scss'
 import { Button } from "../../Shared/Button/Button"
 import { Modal } from "../../Shared/Modal/Modal"
 import { InputField } from "../../Shared/InputField/InputField"
 import { ToastNotification } from "../../Shared/ToastNotification/ToastNotification"
-import { useForm } from "../../../hooks/useForm"
 import { useRooms } from "./RoomsHook"
 import { RoomsList } from "./RoomsList/RoomsList"
-import { floorServiceFactory } from "../../../services/floors"
-import { useParams } from "react-router-dom"
-import { roomServiceFactory } from "../../../services/room"
 import Spinner from "../../Shared/LoadSpinner/LoadSpinner"
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { useSetRooms } from "../../../hooks/Rooms/useSetRooms"
+import { useAddRoom } from "../../../hooks/Rooms/useAddRoom"
+import { useForm } from "../../../hooks/useForm"
+import { useSetFloors } from "../../../hooks/Floors/useSetFloors"
+import { useAddFloor } from "../../../hooks/Floors/useAddFloor"
+import { Floor } from "../../../types/FloorType"
+import { useDeleteFloor } from "../../../hooks/Floors/useDeleteFloor"
 
 export const Rooms = () => {
     
-        const [rooms,setRooms ] = useState<{roomNumber: number,id: string}[]>([])
-        const [roomModal,setRoomModal] = useState(false)
-        const [floorModal, setFloorModal] = useState(false)
-        const [toastText, setToastText] = useState('')
-        const [floors, setFloors ] = useState<{floorNumber:number,id:string }[]>([])
-        const [noRoomsFound, setNoRoomsFound] = useState(false)
-        const [deleteFloorModal, setDeleteFloorModal] = useState(false)
+    const [roomModal,setRoomModal] = useState(false)
+    const [floorModal, setFloorModal] = useState(false)
+    const [deleteFloorModal, setDeleteFloorModal] = useState(false)
+    const [toastText, setToastText] = useState('')
 
-        const floorService = floorServiceFactory()
-        const roomService = roomServiceFactory()
-
-        const params = useParams()
-
-        useEffect(() => {
-                floorService.get(String(params.id))
-                .then( data => setFloors(data.floors))
-                .catch(err => console.error(err))
-        },[])
-    
-        const {formValues,onChangeHandler} = useForm({
+    const {formValues,onChangeHandler} = useForm({
         floorValue: '',
-        roomValue: '',
         roomNumber: '',
         },() => {})
 
-    useEffect (() => {
-        setRooms([])
-        if(formValues.floorValue == ''){
-            return;
-        }
-            const floorId = floors.find(x => String(x.floorNumber) == formValues.floorValue)?.id
-            if(floorId){
-                roomService.get(floorId)
-                .then(data => {
-                    setRooms(data.rooms)
-                    if(data.rooms.length === 0 ){
-                        setNoRoomsFound(true)
-                    }else {
-                        setNoRoomsFound(false)
-                    }
-                })
-            }
-    },[formValues.floorValue])
+    const onAddFail = () => {setToastText('An error occured, please try again later')}
+    const afterAdd = () => {setRoomModal(false)}
+    
+    const { floors,floor,setFloors } = useSetFloors(formValues)
+    const {noRoomsFound,rooms,setRooms} = useSetRooms(floors,formValues) 
+    const {addRoom} = useAddRoom(setRooms,onAddFail,afterAdd)
+
+    const onAddFloorSuccess = (data:Floor) => {
+        setFloors((state) => [...state,data])
+        setFloorModal(false)
+    }
+    const onAddFloorFail = () => {setToastText('An error occured while adding a floor, please try again later')}
+    const {onAddFloor} = useAddFloor(floors.length + 1,onAddFloorSuccess,onAddFloorFail)
+
+    const onDeleteFloorSuccess = () => {
+        setFloors(state => state.filter(x => x.id !== floor!.id))
+        setDeleteFloorModal(false)
+    }
+    const onDeleteFloorFail = () => {setToastText('An error occured while removing this floor, please try again later')}
+    const { deleteFloor } = useDeleteFloor(floor.id,onDeleteFloorSuccess,onDeleteFloorFail)
 
     const {
         onAddRoomClick,
-        onAddRoomHandler,
-        onAddFloor,
-        onDeleteFloor
-    } = useRooms(setRoomModal,formValues,setToastText,setRooms,setFloors,floors,setFloorModal,setDeleteFloorModal)
+    } = useRooms(setRoomModal,formValues,setToastText)
 
     
     return (
@@ -81,7 +68,7 @@ export const Rooms = () => {
                     >Room Number</InputField>
                     <p>Floor: {formValues.floorValue}</p>
                     <br />
-                    <Button width="12rem" disable={formValues.roomNumber == ''} onClick={(event) => onAddRoomHandler(event!)}>Add Room</Button>
+                    <Button width="12rem" disable={formValues.roomNumber == ''} onClick={(event) => addRoom(event!,floor!.id,formValues.roomNumber)}>Add Room</Button>
                 </form>
             </Modal>
             }
@@ -98,7 +85,7 @@ export const Rooms = () => {
             {deleteFloorModal && 
                 <Modal title="Are you sure you want to delete this Floor?" stateSetter={setDeleteFloorModal}>
                     <p className={styles["delete-warning"]} >This will delete the current floor, along with all the rooms inside it.</p>
-                    <Button width="8rem" onClick={onDeleteFloor}>Yes</Button>
+                    <Button width="8rem" onClick={deleteFloor}>Yes</Button>
                     <Button width="8rem" onClick={() => setDeleteFloorModal(false)}>Cancel</Button>
                 </Modal>
             }
