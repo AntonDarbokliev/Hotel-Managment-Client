028f25de0879f5ba1f61faea20e927e029186c2f
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-03-10 19:51:01 +0200

    reserve validations

---
+++ hotel-managment/src/hooks/Validations/useReserveValidations.ts
@@ -0,0 +1,73 @@
+import { checkLengthValidation } from "../../utils/sharedValidations";
+import { useDisableValidations } from "./useDisableValidations";
+
+interface FormValues {
+    [key: string]: string;
+}
+
+interface ValidationValues {
+    [key: string]: boolean;
+}
+
+export const useReserveValidations = (
+    formValues: FormValues,
+    validationValues: ValidationValues
+) => {
+
+    const isFirstNameValid =
+        checkLengthValidation("FirstName",formValues,2) &&
+        validationValues.FirstName === true;
+
+    const isLastNameValid =
+        checkLengthValidation("LastName",formValues, 2) &&
+        validationValues.LastName === true;
+        
+    const isPhoneNumberValid =
+        checkLengthValidation("PhoneNumber",formValues, 5) &&
+        validationValues.PhoneNumber === true &&
+        formValues.PhoneNumber.length <= 15;
+
+    const isCountryValid =
+        checkLengthValidation("Country",formValues, 4) &&
+        validationValues.Country === true;
+
+    const isEGNFPNValid =
+        checkLengthValidation("EGN/FPN",formValues, 10) &&
+        validationValues["EGN/FPN"] === true;
+
+    const isICNValid =
+        checkLengthValidation("ICN",formValues, 5) && validationValues["ICN"] === true;
+
+    const isGenderValid =
+        checkLengthValidation("Gender",formValues, 1) &&
+        validationValues["Gender"] === true;
+
+    const isAddressValid =
+        checkLengthValidation("Address",formValues, 5) &&
+        validationValues.Address === true;
+
+    const validations = [
+        isAddressValid,
+        isCountryValid,
+        isEGNFPNValid,
+        isFirstNameValid,
+        isGenderValid,
+        isICNValid,
+        isLastNameValid,
+        isPhoneNumberValid,
+    ];
+
+    const { disableButton } = useDisableValidations(formValues, validations);
+
+    return {
+        isFirstNameValid,
+        isLastNameValid,
+        isEGNFPNValid,
+        isAddressValid,
+        isCountryValid,
+        isPhoneNumberValid,
+        isGenderValid,
+        isICNValid,
+        disableButton,
+    };
+};
