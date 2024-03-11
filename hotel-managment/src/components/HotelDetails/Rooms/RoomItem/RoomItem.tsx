9414fb50d9e4279611c407a0793a08d54d8cb434
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-03-11 20:37:21 +0200

    Animate room item

--- hotel-managment/src/components/HotelDetails/Rooms/RoomItem/RoomItem.tsx
+++ hotel-managment/src/components/HotelDetails/Rooms/RoomItem/RoomItem.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import styles from './RoomItem.module.scss'
+import { motion } from 'framer-motion';
 
 interface Props {
     number: number,
@@ -14,8 +15,13 @@ export const RoomItem:React.FC<Props> = ({number,id}) => {
         navigate('/room/' + id + '/info')
     }
     return (
-        <div className={styles["room-item"]} onClick={redirect}>
+        <motion.div
+        className={styles["room-item"]} 
+        onClick={redirect}
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.9 }}
+         >
             <h1 >{number}</h1>
-        </div>
+        </motion.div>
     )
 }
