e8dd08897ae88823dd6ee53e62713c79b47643fc
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-03-15 13:20:21 +0200

    day card adjustments

--- hotel-managment/src/components/Shared/Calendar/DayCard/DayCard.tsx
+++ hotel-managment/src/components/Shared/Calendar/DayCard/DayCard.tsx
@@ -6,16 +6,19 @@ interface Props {
     isSelected?: boolean,
     isHighlighted?: boolean,
     hasPassed?: boolean,
+    isBooked?: boolean
 }
 
-export const DayCard: React.FC<Props> = ({day,onClick,isSelected,isHighlighted,hasPassed}) => {
+export const DayCard: React.FC<Props> = ({day,onClick,isSelected,isHighlighted,hasPassed,isBooked}) => {
     return (
-        <div onClick={!hasPassed ? onClick: () => {}} 
+        // <div onClick={!hasPassed ? onClick: () => {}} 
+        <div onClick={ hasPassed || isBooked ?  () => {}: onClick } 
         className={
             `${styles[`day-card`]} 
             ${isSelected ? styles[`selected`] : ''} 
             ${isHighlighted ? styles[`highlighted`] : ''}
             ${hasPassed ? styles[`has-passed`] : ''}
+            ${isBooked ? styles[`is-booked`] : ''}
             `
             }>
             <p>{day}</p>
