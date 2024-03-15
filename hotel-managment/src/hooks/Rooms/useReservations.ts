e94a27122724fccd1a8dda08fe616db59650215a
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-03-15 13:15:43 +0200

    room reservations hook

---
+++ hotel-managment/src/hooks/Rooms/useReservations.ts
@@ -0,0 +1,26 @@
+import { useParams } from "react-router-dom"
+import { reserveServiceFactory } from "../../services/reservation"
+import { useEffect, useState } from "react"
+import { useLoading } from "../useLoading"
+import { RoomReservation } from "../../types/RoomReservation"
+
+const reserveService = reserveServiceFactory()
+
+export const useReservations = () => {
+
+    const [reservations, setReservations ] = useState<RoomReservation[]>([])
+    const roomId = useParams().id!
+    const {isLoading,requestWithLoading} = useLoading()
+  
+        useEffect(() => {
+       
+            requestWithLoading(() => reserveService.getAll(roomId))
+            .then(data => setReservations(data.bookedDates))
+           
+        },[])
+
+        return {
+            reservations,
+            isLoading,
+        }
+}
