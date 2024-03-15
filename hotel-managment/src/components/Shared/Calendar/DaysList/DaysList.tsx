7ac4bf3b031185fd0d1f87362face3c48f657d2d
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-03-15 13:23:02 +0200

    days list overhaul

--- hotel-managment/src/components/Shared/Calendar/DaysList/DaysList.tsx
+++ hotel-managment/src/components/Shared/Calendar/DaysList/DaysList.tsx
@@ -2,37 +2,43 @@ import { Variants, motion } from "framer-motion"
 import { DayCard } from "../DayCard/DayCard"
 import styles from './DaysList.module.scss'
 import { useEffect, useState } from "react";
+import { RoomReservation } from "../../../../types/RoomReservation";
+import { FromTo } from "../../../../types/CalendarFromTo";
+import { useInteractiveCalendar } from "../../../../hooks/Calendar/useInteractiveCalendar";
+
+interface Today {
+  day: number;
+  isMonthNow: boolean;
+  month: number;
+  year: number;
+}
 
 interface Props {
     days: number[],
-    from: {setFrom :React.Dispatch<React.SetStateAction<number>>,from: number },
-    to: {setTo :React.Dispatch<React.SetStateAction<number>>,to: number },
+    from: {setFrom? :React.Dispatch<React.SetStateAction<FromTo | undefined>>,from?: FromTo },
+    to: {setTo? :React.Dispatch<React.SetStateAction<FromTo | undefined>>,to?: FromTo },
     animationVariant?: Variants,
-    today: {day: number, isMonthNow: boolean}
+    today : Today,
+    reservations? : RoomReservation[],
+    month: number,
+    year: number
 }
 
-export const DaysList:React.FC<Props> = ({days,from,to,animationVariant,today}) => {
 
-  const [animationKey, setAnimationKey] = useState(0);
+export const DaysList:React.FC<Props> = ({days,from,to,animationVariant,today,reservations,month,year}) => {
 
+    const [animationKey, setAnimationKey] = useState(0);
 
       useEffect(() => {
           setAnimationKey(animationKey + 1);
       }, [days]);
 
-    const handleFrom = (day:number) => {
-        from.setFrom(day)
+
+      const hasDayPassed = (today: Today,day: number) => {
+       return today.isMonthNow ? today.day > day : false
       }
 
-      const handleTo = (day:number) => {
-        if(day > from.from) {
-          to.setTo(day)
-        }
-        else {
-          from.setFrom(day)
-        }
-      }
-
+    const {handleDayClick,isDayBooked,isDayHighlighted,isDaySelected} = useInteractiveCalendar(from,to,month,year,reservations)
 
     return (
           <motion.div
@@ -43,23 +49,16 @@ export const DaysList:React.FC<Props> = ({days,from,to,animationVariant,today}) 
           exit="hidden"
           key={animationKey}
           >
-                {days.map(day => <DayCard 
-                isHighlighted={ !isNaN(to.to) && day > from.from && day < to.to } 
-                isSelected={ day == from.from || day === to.to} 
-                hasPassed= {today.isMonthNow ? today.day > day : false}
-                onClick={() =>{ 
-                  if(!isNaN(from.from) && !isNaN(to.to)){
-                    from.setFrom(NaN)
-                    to.setTo(NaN)
-                    return  handleFrom(day)
-                  } else if(isNaN(from.from)) {
-                    return  handleFrom(day)
-                  }else {
-                    return  handleTo(day)
-                  }
-                  }} 
-                key={day} 
-                day={day}/>)}
+            {days.map(day => 
+            <DayCard 
+            isHighlighted={isDayHighlighted(day)} 
+            isSelected={ isDaySelected(day)} 
+            hasPassed= {hasDayPassed(today,day)}
+            isBooked = {isDayBooked(day)}
+            onClick={ () => handleDayClick(day)} 
+            key={day} 
+            day={day}
+            />)}
           </motion.div>
     )
 }
