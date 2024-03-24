dc06b6860a8dc274b67f75dd883537762bc4d58b
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-03-24 20:55:27 +0200

    Reservation details

--- hotel-managment/src/components/Reservation/ReservationDetails/ReservationDetails.tsx
+++ hotel-managment/src/components/Reservation/ReservationDetails/ReservationDetails.tsx
@@ -4,6 +4,19 @@ import { useParams } from "react-router-dom"
 import { useLoading } from "../../../hooks/useLoading"
 import { ReceivedRoomReservation } from "../../../types/ReceivedRoomReservation"
 import Spinner from "../../Shared/LoadSpinner/LoadSpinner"
+import styles from './ReservationDetails.module.scss'
+import { InfoField } from "../../Shared/InfoField/InfoField"
+import 
+{
+    faUser,
+    faCircleInfo,
+    faCalendarDays,
+    faEarthAmericas,
+    faLocationDot,
+    faMarsAndVenus,
+    faIdCard
+} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 const reservationService = reserveServiceFactory()
 
@@ -15,7 +28,6 @@ export const ReservationDetails = () => {
     useEffect(() => {
         requestWithLoading(() => reservationService.getOne(params.reservationId!))
         .then(data => setReservation(data.reservation))
-        .then( () => console.log(reservation))
     },[])
 
     return (
@@ -24,10 +36,38 @@ export const ReservationDetails = () => {
             <Spinner></Spinner>
         }
 
-        {!isLoading && reservation !== undefined &&
-        <>
-            <h1>{reservation!.guest.firstName} {reservation!.guest.lastName}</h1>
-        </> 
+        {!isLoading && reservation &&
+        <div className={styles["container"]}>
+            <InfoField height="60%" width="50%">
+                <ul className={styles["info-list"]}>
+
+                    <FontAwesomeIcon className={`${styles["icon"]} ${styles['full-width']}`} size="2x"  icon={faCircleInfo}/>
+
+                    <li> <FontAwesomeIcon icon={faCalendarDays}/> From: {reservation.from}</li>
+                    <li> <FontAwesomeIcon icon={faCalendarDays}/> To: {reservation.to}</li>
+                    <li>ID: {reservation.id}</li>
+
+                    <FontAwesomeIcon className={`${styles["icon"]} ${styles['full-width']}`} size="3x"  icon={faUser}/>
+
+                    <li> <FontAwesomeIcon icon={faIdCard}/> Name: {reservation.guest.firstName} {reservation.guest.lastName}</li>
+                    <li> <FontAwesomeIcon icon={faIdCard}/> EGN: {reservation.guest.egn}</li>
+                    <li> <FontAwesomeIcon icon={faIdCard}/> IDN: {reservation.guest.identityDocumentNumber}</li>
+                    <li> <FontAwesomeIcon icon={faMarsAndVenus}/> Gender: {reservation.guest.gender}</li>
+                    <li> <FontAwesomeIcon icon={faEarthAmericas}/> Country: {reservation.guest.country}</li>
+                    {reservation.guest.emailAddress && 
+                    <li>Email: {reservation.guest.emailAddress}</li>
+                    }
+                    <li> <FontAwesomeIcon icon={faLocationDot}/> Address: {reservation.guest.address}</li>
+
+                    {reservation.additionalInformation && 
+                    <>
+                        <h2>Additional Information</h2>
+                        <li>{reservation.additionalInformation}</li>
+                    </>
+                    }
+                </ul>
+            </InfoField>
+        </div>
         }
         </>
     )
