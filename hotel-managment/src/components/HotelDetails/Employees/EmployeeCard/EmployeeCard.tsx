672e5bbe7b7b12cb2f5a426eca4f0bcb217f1856
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-03-29 14:44:13 +0200

    Employee card

--- hotel-managment/src/components/HotelDetails/Employees/EmployeeCard/EmployeeCard.tsx
+++ hotel-managment/src/components/HotelDetails/Employees/EmployeeCard/EmployeeCard.tsx
@@ -1,21 +1,34 @@
-import { Employee } from "../../../../types/Employee"
 import { Button } from "../../../Shared/Button/Button"
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import styles from './EmployeeCard.module.scss'
+import { useState } from "react"
+import { EmployeeDetails } from "../Modals/EmployeeDetails/EmployeeDetails"
+import { AnimatePresence } from "framer-motion"
+import { ReceivedEmployee } from "../../../../types/ReceivedEmployee"
 
 interface Props {
-    employee: Employee
+    employee: ReceivedEmployee,
 }
 
 export const EmployeeCard: React.FC<Props> = ({employee}) => {
+    const [employeeDetails, setEmployeeDetails] = useState(false)
+
     return (
-        <div className={styles["employee-card"]}>
-            <div className={styles["employee-info"]}>
-                <p>{employee.firstName} {employee.lastName}</p>
-                <p className="highlight">Manager</p>
+        <>
+            <AnimatePresence>
+                {employeeDetails &&
+                    <EmployeeDetails employee={employee} modalSetter={setEmployeeDetails}/>
+                }
+            </AnimatePresence>
+            <div className={styles["employee-card"]} onClick={() => setEmployeeDetails(true)}>
+                
+                <div className={styles["employee-info"]}>
+                    <p>{employee.firstName} {employee.lastName}</p>
+                    <p className="highlight">{employee.role}</p>
+                </div>
+                <Button><FontAwesomeIcon icon={faTrashCan}/></Button>
             </div>
-            <Button><FontAwesomeIcon icon={faTrashCan}/></Button>
-        </div>
+        </>
     )
 }
