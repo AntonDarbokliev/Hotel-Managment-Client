22fa96f16a1b00f8b7e5a52884894bd4659d95ba
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-03-29 14:41:52 +0200

    types

---
+++ hotel-managment/src/types/Employee.ts
@@ -0,0 +1,9 @@
+export interface Employee {
+    firstName:string,
+    middleName: string,
+    lastName: string,
+    EGN: string,
+    Address: string,
+    Role: string,
+    id? : string
+}
---
+++ hotel-managment/src/types/ReceivedEmployee.ts
@@ -0,0 +1,9 @@
+export interface ReceivedEmployee {
+    firstName:string,
+    middleName: string,
+    lastName: string,
+    egn: string,
+    address: string,
+    role: string,
+    id: string
+}
