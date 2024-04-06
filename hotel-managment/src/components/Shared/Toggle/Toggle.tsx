7a2381a14edccf1d193b1e340d0a24e4749d2906
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-06 19:45:16 +0300

    Toggle improvement

--- hotel-managment/src/components/Shared/Toggle/Toggle.tsx
+++ hotel-managment/src/components/Shared/Toggle/Toggle.tsx
@@ -2,15 +2,25 @@ import styles from './Toggle.module.scss';
 import { motion } from "framer-motion";
 
 interface Props {
-    toggleSetter: React.Dispatch<React.SetStateAction<boolean>>,
-    toggleState: boolean
+    toggleSetter?: React.Dispatch<React.SetStateAction<boolean>>,
+    toggleState: boolean,
+    onToggle?: () => void,
 }
 
-export const Toggle: React.FC<Props> = ({toggleSetter,toggleState}) => {
+export const Toggle: React.FC<Props> = ({toggleSetter,toggleState,onToggle}) => {
+    
+        const onClick = () => {
+            if(toggleSetter){
+                toggleSetter(s => !s)
+            }
+            if(onToggle){
+                onToggle()
+            }
+        };
     
     return (
         <div className={`${styles["toggle-container"]}  ${toggleState ? styles.toggled : styles.untoggled}`}>
-           <div className={`${styles['toggle']}`} onClick={() => toggleSetter(s => !s)}>
+           <div className={`${styles['toggle']}`} onClick={onClick}>
                 <motion.div className={styles["toggle-ball"]} layout transition={spring}/>
            </div>
         </div>
