6acaee836b78a4498d897c6804b8652c4a4021e3
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-06 19:44:13 +0300

    Report card

---
+++ hotel-managment/src/components/HotelDetails/Employees/Modals/EmployeeDetails/ReportCard/ReportCard.tsx
@@ -0,0 +1,18 @@
+import { SalaryReport } from "../../../../../../types/SalaryReport";
+import styles from './ReportCard.module.scss';
+
+interface Props {
+    report: SalaryReport 
+}
+
+export const ReportCard:React.FC<Props> = ({report}) => {
+    return (
+        <div className={styles["salary-report"]}>
+            <table>
+             <tr> <th>Salary</th> <td>{report.salary}</td></tr>  
+             <tr> <th>Report Date</th> <td>{String(report.date)}</td></tr>  
+             <tr> <th>Has been payed</th> <td>{report.isPaid ? 'Yes': 'No'}</td></tr>  
+            </table>
+        </div>
+    )
+    }
