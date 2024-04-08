56dffca05b97e525971101f9e781a70d68e4d0cd
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-08 17:08:07 +0300

    update room add fields

--- hotel-managment/src/components/HotelDetails/Rooms/modals/AddRoomModal/AddRoomModal.tsx
+++ hotel-managment/src/components/HotelDetails/Rooms/modals/AddRoomModal/AddRoomModal.tsx
@@ -1,10 +1,13 @@
 import { useAddRoom } from "../../../../../hooks/Rooms/useAddRoom";
 import { useForm } from "../../../../../hooks/useForm";
+import { useFormValidation } from "../../../../../hooks/useFormValidation";
 import { useToastStore } from "../../../../../stores/ToastStore";
 import { Floor } from "../../../../../types/FloorType";
+import { InputFieldType } from "../../../../../types/InputField";
+import { Room } from "../../../../../types/RoomType";
 import { SentRoom } from "../../../../../types/SentRoom";
 import { Button } from "../../../../Shared/Button/Button";
-import { InputField } from "../../../../Shared/InputField/InputField";
+import { InputFieldslist } from "../../../../Shared/InputFieldsList/InputFieldsList";
 import Spinner from "../../../../Shared/LoadSpinner/LoadSpinner";
 import { Modal } from "../../../../Shared/Modal/Modal"
 import styles from './AddRoomModal.module.scss';
@@ -20,30 +23,42 @@ export const AddRoomModal:React.FC<Props> = ({modalSetter,roomSetter,floor}) => 
     
     const {formValues,onChangeHandler} = useForm({
         roomNumber: '',
+        pricePerNight: '',
+        peopleCapacity: '',
         },() => {})
 
     const toastSetter = useToastStore(s => s.setToastText)
 
-    const onAddFail = () => {toastSetter('An error occured, please try again later')}
-    const afterAdd = () => {modalSetter(false)}
-    const {addRoom,isLoading} = useAddRoom(roomSetter,onAddFail,afterAdd)
+    const onAddFail = (error:string) => {toastSetter(error)}
+    const afterAdd = (data: {room: Room}) => {
+        modalSetter(false);
+        roomSetter(state => [...state,data.room])
+    }
+    const {addRoom,isLoading} = useAddRoom(onAddFail,afterAdd)
 
-
+    const inputs = [
+        { name: 'roomNumber',type: 'number', display: 'Room Number'},
+        { name: 'pricePerNight',type: 'number', display: 'Price per night'},
+        { name: 'peopleCapacity',type: 'number', display: 'Capacity'},
+    ] as InputFieldType[]
+    const { onBlurHandler,onFocusHandler}  =useFormValidation({})
     return (
         <Modal key={'room-modal'} stateSetter={modalSetter} title="Add a Room">
             {!isLoading && 
             <>
                 <form action="" className={styles["room-modal-form"]}>
-                    <InputField type="number" 
-                    name="roomNumber" 
-                    value={formValues.roomNumber}
-                    onChange={onChangeHandler}
-                    >Room Number</InputField>
+                    <InputFieldslist 
+                    onChangeHandler={onChangeHandler} 
+                    formValues={formValues} 
+                    inputs={inputs}
+                    onBlurHandler={onBlurHandler}
+                    onFocusHandler={onFocusHandler}
+                    />
                     <p>Floor: {floor.floorNumber}</p>
                     <br />
                 </form>
-                    <Button width="12rem" disable={formValues.roomNumber == ''} 
-                    onClick={() => addRoom(floor.id,formValues.roomNumber)}>
+                    <Button width="12rem"  disable = {formValues.roomNumber == ''}
+                    onClick={() => addRoom(floor.id,formValues)}>
                     Add Room
                     </Button>
             </>
