816cf96c459c0f82e2fb16b65d98c3fe34daaa56
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-08 17:07:51 +0300

    improve add room

--- hotel-managment/src/hooks/Rooms/useAddRoom.ts
+++ hotel-managment/src/hooks/Rooms/useAddRoom.ts
@@ -1,26 +1,28 @@
 import { roomServiceFactory } from "../../services/room";
+import { ErrorObj } from "../../types/ErrorTypes";
+import { Room } from "../../types/RoomType";
+import { extractErrors } from "../../utils/extractErrors";
+import { makeFormData } from "../../utils/makeFormData";
 import { useLoading } from "../useLoading";
 
 export const useAddRoom = (
-    roomSetter: React.Dispatch<React.SetStateAction<{roomNumber: number;id: string;}[]>>,
-    onAddFail: () => void, 
-    afterAdd: () => void 
+    onAddFail: (text:string) => void, 
+    afterAdd: (data: {room: Room}) => void 
     ) => {
     
     const roomService = roomServiceFactory()
     const {isLoading,requestWithLoading} = useLoading()
 
-    const addRoom = async (floorId:string,roomNumber: string | number) => {
-        const formData = new FormData()
-        formData.append('RoomNumber',String(roomNumber))
-        formData.append('FLoorId',floorId)
+    const addRoom = async (floorId:string,formValues: {[key:string] : string}) => {
+        const formData = makeFormData(formValues)
+        formData.append('FloorId',floorId)
         try {
             const data = await requestWithLoading( () => roomService.add(formData))
-            roomSetter(state => [...state,data.room])
+            afterAdd(data)
         }catch(error) {
-            onAddFail()
+            const errorTxt = extractErrors(error as ErrorObj) 
+            onAddFail(errorTxt)
         }
-        afterAdd()
         
     }
 
