8e9dbb3968eb22dfc109164dd300074f853a300f
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-10 17:48:55 +0300

    Account page testing

--- hotel-managment/src/tests/components/Settings/Accounts.test.tsx
+++ hotel-managment/src/tests/components/Settings/Accounts.test.tsx
@@ -1,12 +1,29 @@
-import { it, expect, describe } from 'vitest'
+import { it, expect, describe,vi,beforeAll } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import { Account } from '../../../components/Settings/Account/Account'
 import { BrowserRouter } from 'react-router-dom'
-// import { useAuthStore } from '../../../stores/Auth'
+
+
+vi.mock("../../../stores/Auth", () => ({
+    useAuthStore: (passedFunction: (data: object) => void) => {
+        const data = {
+            user: {
+                fullName: 'Anton Darbokliev',
+                role: "Owner",
+                picture: "",
+                id: "",
+            },
+            updateUser: vi.fn(),
+            clearUser: vi.fn(),
+        }
+
+        return passedFunction(data);
+    }
+}));
 
 describe('Account Testing', () => {
-    it('renders component', () => {
-        // const user = useAuthStore(s => s.)
+   
+    beforeAll(() => {
         render((
             <>
             <BrowserRouter>
@@ -14,9 +31,23 @@ describe('Account Testing', () => {
             </BrowserRouter>
             </>
         )) 
-        const header = screen.getByText('Name')
-        expect(header).toBeVisible()
-        expect(header).toHaveTextContent('Name:')
+    })
+
+    it('renders component', async () => {
+        const resetOption =  screen.getByText('Reset',{exact: false})
+
+        expect(resetOption).toBeInTheDocument()
+
+    })
+
+
+    it('shows user info', async () => {
+      
+        const role =  screen.getByText('Owner',{exact: false})
+        const name =  screen.getByText('Anton Darbokliev',{exact: false})
+
+        expect(role).toBeInTheDocument()
+        expect(name).toBeInTheDocument()
 
     })
 })
