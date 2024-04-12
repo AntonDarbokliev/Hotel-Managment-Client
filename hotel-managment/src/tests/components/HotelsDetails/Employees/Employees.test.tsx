dce70c0b31c8cba5d6a9d7c5bd7c6ee7974edbe7
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-12 15:06:07 +0300

    Employees page testing

--- hotel-managment/src/tests/components/HotelsDetails/Employees/Employees.test.tsx
+++ hotel-managment/src/tests/components/HotelsDetails/Employees/Employees.test.tsx
@@ -1,37 +1,56 @@
-import { render,screen, waitFor } from '@testing-library/react'
-import { it, expect, describe } from 'vitest'
-import { Employees } from '../../../../components/HotelDetails/Employees/Employees'
-import userEvent from '@testing-library/user-event'
-import { act } from 'react-dom/test-utils'
+import { RenderResult, render, screen, waitFor } from "@testing-library/react";
+import { it, expect, describe } from "vitest";
+import { Employees } from "../../../../components/HotelDetails/Employees/Employees";
+import userEvent from "@testing-library/user-event";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
 
-
-describe('Employees page', () => {
+let lastMount: RenderResult;
+describe("Employees page", () => {
     beforeAll(() => {
-        localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJhZGEzMjc2NC00NDA5LTRkNmUtOTgzMi1mMzFlYTY4M2Y4MjAiLCJQcm9maWxlUGljdHVyZSI6Imh0dHBzOi8vcmVzLmNsb3VkaW5hcnkuY29tL2R4eGp5cWJ6dC9yYXcvYXV0aGVudGljYXRlZC9zLS1QX2k4QWZjci0tL3YxNzExNTY2NzMxL1Byb2ZpbGVQaWN0dXJlcy83MTdkYThmNC01N2ViLTQxOTEtODg3Yi03ZmY5NGJmZjI3ZjYucG5nIiwiRnVsbE5hbWUiOiJBbnRvbiBEYXJib2tsaWV2Iiwicm9sZSI6Ik93bmVyIiwibmJmIjoxNzEyODUxMDMwLCJleHAiOjE3MTI4NjE4MzAsImlhdCI6MTcxMjg1MTAzMCwiaXNzIjoibXlpc3N1ZXIiLCJhdWQiOiJteWF1ZGllbmNlIn0.2A27wbbW3TvumaWIduKbp2JjBxS_5T1jyuhTraPqKHw')        
-    })
-
+        localStorage.setItem(
+            "token",
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJhZGEzMjc2NC00NDA5LTRkNmUtOTgzMi1mMzFlYTY4M2Y4MjAiLCJQcm9maWxlUGljdHVyZSI6Imh0dHBzOi8vcmVzLmNsb3VkaW5hcnkuY29tL2R4eGp5cWJ6dC9yYXcvYXV0aGVudGljYXRlZC9zLS1QX2k4QWZjci0tL3YxNzExNTY2NzMxL1Byb2ZpbGVQaWN0dXJlcy83MTdkYThmNC01N2ViLTQxOTEtODg3Yi03ZmY5NGJmZjI3ZjYucG5nIiwiRnVsbE5hbWUiOiJBbnRvbiBEYXJib2tsaWV2Iiwicm9sZSI6Ik93bmVyIiwibmJmIjoxNzEyODUxMDMwLCJleHAiOjE3MTI4NjE4MzAsImlhdCI6MTcxMjg1MTAzMCwiaXNzIjoibXlpc3N1ZXIiLCJhdWQiOiJteWF1ZGllbmNlIn0.2A27wbbW3TvumaWIduKbp2JjBxS_5T1jyuhTraPqKHw"
+        );
+    });
 
     beforeEach(() => {
-        render(<Employees/>)
-    })
+        lastMount = render(<Employees />);
+    });
 
-    it('renders', () => {
-        expect(screen.getByText('Employees')).toBeInTheDocument()
-        expect(screen.getByRole('button',{name: /add/i})).toBeInTheDocument()
-    })
+    afterEach(() => {
+        lastMount.unmount();
+    });
 
-    it('opens add an employee modal',async () => {
-        const user = userEvent.setup()
+    it("renders", () => {
+        expect(screen.getByText("Employees")).toBeInTheDocument();
+        expect(screen.getByRole("button", { name: /add/i })).toBeInTheDocument();
+    });
 
-        act( async () => {
-            await user.click(screen.getByTestId('add'))
-        })
+    it("opens add an employee modal", async () => {
+        lastMount.unmount();
+        const router = createMemoryRouter(
+            [
+                {
+                    path: "/hotels/:id/employees",
+                    element: <Employees />,
+                },
+            ],
+            {
+                initialEntries: ["/hotels/123456789/employees"],
+            }
+        );
+        render(<RouterProvider router={router} />);
+        const user = userEvent.setup();
+
+        user.click(screen.getByText(/add/i));
 
         await waitFor(() => {
-            screen.getByText(/role/i)
-        })
-        expect(screen.getByRole('button',{name:/add/i})).toBeInTheDocument()
-        expect(screen.getByText(/name/i)).toBeInTheDocument()
-        expect( screen.getByText(/role/i)).toBeInTheDocument()
-    })
-})
+            screen.getByText(/x/i);
+        });
+
+        expect(screen.getByText(/x/i)).toBeInTheDocument();
+        expect(screen.getByText(/salary/i)).toBeInTheDocument();
+        expect(screen.getByText(/address/i)).toBeInTheDocument();
+        expect(screen.getByText(/phoneNumber/i)).toBeInTheDocument();
+    });
+});
