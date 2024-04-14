6d36d8d0bc5159b29906808e25e16a36246f3ead
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-14 14:45:01 +0300

    use loading changes

--- hotel-managment/src/hooks/useLoading.ts
+++ hotel-managment/src/hooks/useLoading.ts
@@ -1,24 +1,18 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const useLoading = () => {
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const requestWithLoading = async (callback: () => Promise<any>) => {
-        setIsLoading(true)
-        try {
-            const result = await callback()
-            return result
-        } catch (error) {
-            console.log('Error from loading',error)
-            throw error
-    }finally{
-        setIsLoading(false)
-    }
-    }
+        setIsLoading(true);
+        const result = await callback();
+        setIsLoading(false);
+        return result;
+    };
 
     return {
         isLoading,
-        requestWithLoading
-    }
-}
+        requestWithLoading,
+    };
+};
