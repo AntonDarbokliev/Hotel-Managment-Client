105a7ccfa0c60f3fc225390123767ca4eb9ee136
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-16 16:04:16 +0300

    hotel adjustments

--- hotel-managment/src/types/HotelTypes.ts
+++ hotel-managment/src/types/HotelTypes.ts
@@ -3,5 +3,14 @@
     email: string,
     address: string,
     profilePicture: string 
-    id: string 
+    id: string ,
+    telephoneNumber: string
+}
+
+export interface HotelSendType {
+    name:string,
+    email: string,
+    address: string,
+    profilePicture: File ,
+    telephoneNumber: string,
 }
