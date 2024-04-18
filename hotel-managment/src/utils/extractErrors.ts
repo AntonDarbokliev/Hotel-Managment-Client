3e2db3532c9ca19cab58e920e101107e809867de
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-18 20:28:07 +0300

    extract errors adjustment

--- hotel-managment/src/utils/extractErrors.ts
+++ hotel-managment/src/utils/extractErrors.ts
@@ -1,8 +1,11 @@
-import { ErrorObj } from "../types/ErrorTypes"
-
+import { ErrorObj } from "../types/ErrorTypes";
 
 export const extractErrors = (errorObj: ErrorObj) => {
-        let text = ''
-        Object.values(errorObj.errors).forEach((e) => e.forEach(x => text += `${x}\n`))
+    let text = "";
+    if (errorObj.error) {
+        text = errorObj.error
         return text
     }
+    Object.values(errorObj.errors).forEach((e) => e.forEach((x) => (text += `${x}\n`)));
+    return text;
+};
