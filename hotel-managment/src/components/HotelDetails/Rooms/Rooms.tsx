73085f58b8c0011a382064f4aac8692e243f5c53
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-18 20:29:55 +0300

    search in rooms

--- hotel-managment/src/components/HotelDetails/Rooms/Rooms.tsx
+++ hotel-managment/src/components/HotelDetails/Rooms/Rooms.tsx
@@ -16,6 +16,8 @@ import { useToastStore } from "../../../stores/ToastStore"
 import { AddRoomModal } from "./modals/AddRoomModal/AddRoomModal"
 import { AddFloorModal } from "./modals/AddFloorModal/AddFloorModal"
 import { DeleteFloorModal } from "./modals/DeleteFloorModal/DeleteFloorModal"
+import { SearchBar } from "../../Shared/SearchBar/SearchBar"
+import { useSearchInRooms } from "../../../hooks/Rooms/useSearchInRooms"
 
 export const Rooms = () => {
     
@@ -23,6 +25,7 @@ export const Rooms = () => {
     const [floorModal, setFloorModal] = useState(false)
     const [deleteFloorModal, setDeleteFloorModal] = useState(false)
     const setToastText = useToastStore(s => s.setToastText)
+    const [searchValue, setSearchValue] = useState('')
 
 
     const {formValues,onChangeHandler} = useForm({
@@ -31,6 +34,8 @@ export const Rooms = () => {
 
     const { floors,floor,setFloors } = useSetFloors(formValues)
     const {noRoomsFound,rooms,setRooms} = useSetRooms(floors,formValues) 
+    const {searchedRooms} = useSearchInRooms(searchValue,rooms)
+
 
     const {
         onAddRoomClick,
@@ -64,18 +69,18 @@ export const Rooms = () => {
             <h1>Rooms</h1>
             <div className={styles["dropdowns"]}>
                 <Dropdown onChange={onChangeHandler} name="floorValue" value={formValues.floorValue}>
-                    <option>Select a Floor</option>
+                    <option value=''>Select a Floor</option>
                     {floors.map(floor => <option key={floor.id}>{floor.floorNumber}</option>) }
                 </Dropdown>
+                <SearchBar searchText={searchValue} setSearchText={setSearchValue}/>
                 {formValues.floorValue !== '' && 
-                <Button onClick={() => setDeleteFloorModal(true)}>
+                <Button width="2.3rem" onClick={() => setDeleteFloorModal(true)}>
                     <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon>
                 </Button>}
             </div>
 
-
             { rooms.length > 0 && 
-                <RoomsList rooms={rooms}/>
+                <RoomsList rooms={searchedRooms && searchedRooms?.length > 0 ? searchedRooms :rooms}/>
             }
 
             { rooms.length == 0 && !noRoomsFound && formValues.floorValue != '' && 
@@ -86,9 +91,6 @@ export const Rooms = () => {
                 <p>Select a floor to view rooms</p>
             }
 
-            {/* {noRoomsFound && 
-                <p>No rooms added yet</p>
-            } */}
             <div className={styles["buttons"]}>
                 <Button width="90%" onClick={() => setFloorModal(true)}>Add a Floor</Button>
                 <Button width="90%" onClick={onAddRoomClick}>Add a Room</Button>
