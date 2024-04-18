f4311a30c59af243d0497bdd76b2d87911736ec1
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-18 20:29:37 +0300

    search in employees

--- hotel-managment/src/components/HotelDetails/Employees/EmployeeList/EmployeeList.tsx
+++ hotel-managment/src/components/HotelDetails/Employees/EmployeeList/EmployeeList.tsx
@@ -1,13 +1,23 @@
 import { useEmployeeStore } from "../../../../stores/EmployeeStore"
+import { ReceivedEmployee } from "../../../../types/ReceivedEmployee"
 import { EmployeeCard } from "../EmployeeCard/EmployeeCard"
 import styles from './EmployeeList.module.scss'
 
-export const EmployeeList = () => {    
+interface Props {
+    searchedEmployees?: ReceivedEmployee[]
+}
+
+export const EmployeeList:React.FC<Props> = ({searchedEmployees}) => {    
     const {employees} = useEmployeeStore()
-    
+
     return (
         <div className={styles["employee-list"]}>
-            {employees?.map(emp => <EmployeeCard  key={emp.id} employee={emp}/>)}
+            {searchedEmployees !== undefined && searchedEmployees.length > 0 &&
+             searchedEmployees?.map(emp => <EmployeeCard  key={emp.id} employee={emp}/>)
+            }
+            {( searchedEmployees == undefined || searchedEmployees.length == 0) &&
+             employees?.map(emp => <EmployeeCard  key={emp.id} employee={emp}/>)
+             }
         </div>
     )
 }
